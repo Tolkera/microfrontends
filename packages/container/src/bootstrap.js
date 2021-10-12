c22ae54abbeb83abe734af3af796ddc52736f9e5
Iryna <tolkera@tolkera>
2021-10-12 11:04:12 +0200

    Updated workflow

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import App from './App';
 
-console.info('bootstrapping 5');
+console.info('bootstrapping 6');
 ReactDOM.render(<App />, document.querySelector('#root'))
