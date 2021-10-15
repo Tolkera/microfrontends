510c3f2037aa179132db09eec651a5ca3f063429
Iryna <tolkera@tolkera>
2021-10-15 11:23:02 +0200

    Added dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,22 +1,31 @@
-import React, {lazy, Suspense, useState } from "react";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, {lazy, Suspense, useState, useEffect } from "react";
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import Header from './components/Header';
 import Progress from "./components/Progress";
 import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co'
 })
+import {createBrowserHistory} from 'history';
 const MarketingLazy = lazy(()=>{
     return import('./components/MarketingApp')
 })
 const AuthLazy = lazy(()=>{
     return import('./components/AuthApp')
 })
-
+const DashboardLazy = lazy(()=>{
+    return import('./components/DashboardApp')
+})
+const history = createBrowserHistory();
 export default() => {
     const [isSignedIn, setIsSignedIn] = useState(false);
+    useEffect(()=>{
+       if(isSignedIn){
+           history.push('/dashboard');
+       }
+    }, [isSignedIn])
     return(
-    <BrowserRouter>
+    <Router history={history}>
         <StylesProvider generateClassName={generateClassName}>
         <div>
             <Header isSignedIn={isSignedIn} onSignOut={()=>{setIsSignedIn(false)}}/>
@@ -25,12 +34,16 @@ export default() => {
                     <Route path="/auth" component={AuthLazy}>
                         <AuthLazy onSignin = {()=>{setIsSignedIn(true)}} />
                     </Route>
+                    <Route path="/dashboard">
+                        {!isSignedIn && <Redirect to='/' />}
+                        <DashboardLazy />
+                    </Route>
                     <Route path="/" component={MarketingLazy}></Route>
                 </Switch>
             </Suspense>
 
         </div>
         </StylesProvider>
-    </BrowserRouter>
+    </Router>
     )
 }
